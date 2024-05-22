3eb66ffb07e9c0c9d88233e3822f2b052a3bc145
chinatsu.honda <c-honda@i-enter.co.jp>
2024-05-22 04:23:05 +0000

    follow, unfollow

--- routes/web.php
+++ routes/web.php
@@ -2,25 +2,23 @@
 
 // use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\MicropostsController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\UserFollowController;
 
 Route::get('/', [MicropostsController::class, 'index']);
 
 Route::get('/dashboard',[MicropostsController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::prefix('users/{id}')->group(function () {
+        Route::post('follow', [UserFollowController::class, 'store'])->name('user.follow');
+        Route::delete('unfollow', [UserFollowController::class, 'destroy'])->name('user.unfollow');
+        Route::get('followings', [UsersController::class, 'followings'])->name('users.followings');
+        Route::get('followers', [UsersController::class, 'followers'])->name('users.followers');
+    });
+    
     Route::resource('users', UsersController::class, ['only' => ['index', 'show']]);
     // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
